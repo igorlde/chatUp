49ff3bb31dcc80a772a1ecc938b79cefc85f901f
igorlde <igorligeiro@gmail.com>
2025-04-04 18:49:23 -0300

    Pasta style adicionada

--- main.php
+++ main.php
@@ -4,30 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Web_principal_chatUp</title>
-    <style>
-        .btn-criar-post {
-            display: inline-block;
-            padding: 12px 24px;
-            background-color: #4CAF50;
-            color: white;
-            text-decoration: none;
-            border-radius: 5px;
-            font-size: 16px;
-            margin: 20px;
-            transition: background-color 0.3s;
-            border: none;
-            cursor: pointer;
-        }
-
-        .btn-criar-post:hover {
-            background-color: #45a049;
-        }
-        
-
-        h1{
-            color: red;
-        }
-    </style>
+    <link rel="stylesheet" href="style/style.css">
 </head>
 <body>
     <h1>Testando</h1>
