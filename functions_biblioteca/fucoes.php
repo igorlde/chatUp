4be461281cfea7f0d6f60f8707ee884cc15864ff
igorlde <igorligeiro@gmail.com>
2025-04-30 20:00:01 -0300

    Style anna projeto very nice

---
+++ functions_biblioteca/fucoes.php
@@ -0,0 +1,10 @@
+<?php 
+include("../connector_database/connector.php");
+//adicionar livro
+function addLivro($admim, $conn, $titulo, $autor){
+
+}
+function addCategoria($nome, $descricao, $admim){
+
+}
+?>
