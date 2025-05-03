a3babde2d08131c9c9a17399f42af43e194e2352
igorlde <igorligeiro@gmail.com>
2025-05-03 11:42:38 -0300

    mudança besta tinha esquecido de colocar editar-perfil.php dentro de menus

--- visualizar-html/sideBarMenu.php
+++ visualizar-html/sideBarMenu.php
@@ -10,7 +10,7 @@
 <body>
     <!--criar metodo de butao para ver lista de conf-->
     <div>
-        <a href="editar-perfil.php"><button type="submit">Edita perfil</button></a>
+        <a href="menus/editar-perfil.php"><button type="submit">Edita perfil</button></a>
         <a href="menus/blocoNotas.php"><button type="submit">Bloco de notas</button></a>
         <a href="#"><button type="submit"></button></a>
 
